e60bee01290369db0d2049aefbe40d81584829ae
Sean <sean.tbear@gmail.com>
2020-12-29 16:09:06 -0500

    added flask-cors

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from flask_restful import Api
 
+from flask_cors import CORS, cross_origin
+
 # Import the class in todo.py with get/put endpoints
 # from resources.todo import todo
 from resources import todo
 Todo = todo.Todo
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 api = Api(app)
 
 api.add_resource(Todo, "/todo/<int:id>")
